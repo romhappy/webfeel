ab63dc24f3423c4a7d238b777378bdbab4010984
romhappy <romainbesin21@gmail.com>
2024-11-23 09:42:01 +0100

    maj js

--- script.js
+++ script.js
@@ -1,74 +1,77 @@
 function getSelectValue(selectId) {
     const select = document.getElementById(selectId);
-    return parseInt(select.value, 10); // Retourne la valeur sélectionnée comme un nombre entier
-}
-
-function generateChart() {
-    // Initialisation des données pour chaque domaine
+    return parseInt(select.value, 10);
+  }
+  
+  function generateChart() {
     let domainData = {
-        somatique: {
-            difficulte: getSelectValue("q00difficulte"),
-            urgence: getSelectValue("q00urgence"),
-            aide: getSelectValue("q00aide")
-        },
-        psychique: {
-            difficulte: getSelectValue("q10difficulte"),
-            urgence: getSelectValue("q10urgence"),
-            aide: getSelectValue("q10aide")
-        },
-        social: {
-            difficulte: getSelectValue("q20difficulte"),
-            urgence: getSelectValue("q20urgence"),
-            aide: getSelectValue("q20aide")
-        }
+      somatique: {
+        difficulte: getSelectValue("q00difficulte"),
+        urgence: getSelectValue("q00urgence"),
+        aide: getSelectValue("q00aide")
+      },
+      psychique: {
+        difficulte: getSelectValue("q10difficulte"),
+        urgence: getSelectValue("q10urgence"),
+        aide: getSelectValue("q10aide")
+      },
+      social: {
+        difficulte: getSelectValue("q20difficulte"),
+        urgence: getSelectValue("q20urgence"),
+        aide: getSelectValue("q20aide")
+      }
     };
-
-    // Affichage des données dans la console pour déboguer
+  
     console.log(domainData);
-
+  
     const labels = ["Santé somatique", "Santé psychique", "Lien social"];
-    const somatique = [domainData.somatique.difficulte, domainData.somatique.urgence, domainData.somatique.aide];
-    const psychique = [domainData.psychique.difficulte, domainData.psychique.urgence, domainData.psychique.aide];
-    const social = [domainData.social.difficulte, domainData.social.urgence, domainData.social.aide];
-
+    const difficulte = [domainData.somatique.difficulte, domainData.psychique.difficulte, domainData.social.difficulte];
+    const urgence = [domainData.somatique.urgence, domainData.psychique.urgence, domainData.social.urgence];
+    const aide = [domainData.somatique.aide, domainData.psychique.aide, domainData.social.aide];
+  
     const ctx = document.getElementById("myChart").getContext("2d");
-
     const chart = new Chart(ctx, {
-        type: 'radar',
-        data: {
-            labels: labels,
-            datasets: [
-                {
-                    label: 'Difficulté',
-                    data: somatique,
-                    borderColor: 'rgba(255, 99, 132, 1)',
-                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                    borderWidth: 1
-                },
-                {
-                    label: 'Urgence',
-                    data: psychique,
-                    borderColor: 'rgba(54, 162, 235, 1)',
-                    backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                    borderWidth: 1
-                },
-                {
-                    label: 'Aide en place',
-                    data: social,
-                    borderColor: 'rgba(75, 192, 192, 1)',
-                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                    borderWidth: 1
-                }
-            ]
-        },
-        options: {
-            scales: {
-                r: {
-                    min: 0,
-                    max: 3,
-                    stepSize: 1
-                }
-            }
+      type: 'radar',
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: 'Difficulté',
+            data: difficulte,
+            borderColor: 'rgba(255, 99, 132, 1)',
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderWidth: 1
+          },
+          {
+            label: 'Urgence',
+            data: urgence,
+            borderColor: 'rgba(54, 162, 235, 1)',
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderWidth: 1
+          },
+          {
+            label: 'Aide en place',
+            data: aide,
+            borderColor: 'rgba(75, 192, 192, 1)',
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        scales: {
+          r: {
+            min: 0,
+            max: 3,
+            stepSize: 1
+          }
         }
+      }
     });
-}
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    const generateButton = document.querySelector('button');
+    generateButton.addEventListener('click', generateChart);
+  });
+  
